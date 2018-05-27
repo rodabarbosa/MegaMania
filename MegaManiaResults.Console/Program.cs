44d25a34107df58aeac80054fe4fcdade74bcc84
Rodrigo Barbosa <rodrigoarabarbosa@outlook.com>
2018-05-27 16:50:32 -0300

    Adding Parallel work

---
+++ MegaManiaResults.Console/Program.cs
@@ -0,0 +1,47 @@
+﻿using MegaManiaResults.Core;
+using System;
+
+namespace megamaniaresults
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime start = DateTime.Now;
+            Console.WriteLine("Esta aplicação irá os número mais sorteados da Magamania Paraná");
+            Console.WriteLine("Endereço dos resultados:");
+            try
+            {
+                string url = "http://adm.megamaniadasorte.com.br/consultaresultado/index.php";
+                Console.WriteLine(url);
+                Console.WriteLine();
+
+                var quantity = 30;
+                var manipulator = new WebManipulator(url);
+                manipulator.LoadResults();
+                foreach (var key in manipulator.Results.Keys)
+                {
+                    Console.WriteLine($"{key}: {manipulator.Results[key]}");
+                }
+
+
+                var numbers = manipulator.GetTopNumbers(quantity);
+
+                Console.WriteLine(string.Format("Os {0} números mais sorteados:", quantity));
+                Console.WriteLine("Número \t Quantidade");
+                quantity = numbers.Length;
+                for (var i = 0; i < quantity; i++)
+                {
+                    Console.WriteLine(string.Format("{0} \t {1}", numbers[i], i));
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Tempo consumido: {0}", (DateTime.Now-start));
+            Console.Read();
+        }
+    }
+}
---
+++ MegaManiaResults.Core/WebManipulator.cs
@@ -0,0 +1,73 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaManiaResults.Core
+{
+    public class WebManipulator
+    {
+        private readonly string _baseUrl;
+        public Dictionary<string, int> _results;
+        public Dictionary<string, int> Results { get => this._results; }
+
+        public WebManipulator(string baseUrl)
+        {
+            this._baseUrl = baseUrl;
+            this._results = new Dictionary<string, int>();
+        }
+
+        public void LoadResults()
+        {
+            var startDate = new DateTime(2015, 1, 11);
+            int numberDayToNow = (int)(DateTime.Today - startDate).TotalDays;
+
+            Parallel.For(0, numberDayToNow, index =>
+            {
+                this.GetResultFromUrl($"{this._baseUrl}?DtSorteio={startDate.AddDays(index).ToString("yyyy-MM-dd")}&enviar=Ok");
+            });
+        }
+
+        public string[] GetTopNumbers(int topQuantity = 30)
+        {
+            if (this._results.Count == 0)
+                this.LoadResults();
+
+            return this._results.AsParallel()
+                        .Select(n => n.Key)
+                        .Take(topQuantity)
+                        .ToArray();
+
+        }
+
+        private void GetResultFromUrl(string url)
+        {
+            using (var client = new WebClient())
+            {
+                var htmlString = client.DownloadString(url);
+                this.ExtractResultNumbers(htmlString);
+            }
+        }
+
+        private void ExtractResultNumbers(string htmlString)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(htmlString);
+            var nodes = doc.DocumentNode.SelectNodes("//div[@class='bola']");
+            if (nodes != null)
+            {
+                Parallel.ForEach(nodes, (node) =>
+                {
+                    var value = node.InnerHtml.Trim();
+                    if (this._results.ContainsKey(value))
+                        this._results[value]++;
+                    else
+                        this._results.Add(value, 1);
+                });
+            }
+        }
+    }
+}
